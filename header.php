2a8f28f003bb734e040fc74cd48033c0f3e50bd3
Ulisses <ualbardeiro@gmail.com>
2024-03-08 19:41:39 -0300

    reconstrução do site

---
+++ header.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+<link rel="stylesheet" href="assets/css/header.css">
+
+    
+<header>
+        <nav class="nav-bar">
+            <div class="logo">
+              <img src="assets/img/logo_header.png" alt="logo">
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li class="nav-item"><a href="#" class="nav-link">QUEM SOMOS</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">RESIDENCIAIS</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">CONTATOS</a></li>
+                </ul>
+            </div>
+            <div class="icon-social">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-whatsapp"></i>
+            </div>
+           <!--  <div class="login-button">
+                <button><a href="#">Entrar</a></button>
+            </div> -->
+
+            <div class="mobile-menu-icon">
+                <button onclick="menuShow()"><img class="icon" src="assets/img/menu_white_36dp.svg" alt=""></button>
+            </div>
+        </nav>
+        <div class="mobile-menu">
+            <ul>
+                <li class="nav-item"><a href="#" class="nav-link">QUEM SOMOS</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">RESIDENCIAIS</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">CONTATOS</a></li>
+            </ul>
+
+            <div class="icon-social-mobile">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-whatsapp"></i>
+            </div>
+
+            <!-- <div class="login-button">
+                <button><a href="#">Entrar</a></button>
+            </div> -->
+        </div>
+    </header>
+
+    <script src="assets/js/header.js"></script>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>ENGETEG | CONTRUTORA</title>
+</head>
+<body>
+
+<?php include_once ("header.php") ?>    
+        
+    <div class="bd-example">
+        <div id= "container  carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="banner carousel-item active">
+                    <img src="assets/img/banner1.png" class=" d-block w-100" data-src="holder.js/800x400?auto=yes&amp;bg=777&amp;fg=555&amp;text=Primeiro Slide" alt="Primeiro Slide" />
+                    <div class="
+                    content-banner carousel-caption d-none d-md-block">
+                        <img class="logo-slide" src="assets/img/logo_california.png" alt="">
+                        <h1 class="title-slide">santa HERNESTINA</h5>
+                        <p class="text-slide">AGORA O SONHO DA CASA <br> PRÓPRIA ESTÁ AO SEU ALCANCE</p>
+                        <button class="btn-slide">SAIBA MAIS</button>
+                    </div>
+                </div>
+                <div class="banner carousel-item">
+                    <img src="assets/img/banner2.png" class="d-block w-100" data-src="holder.js/800x400?auto=yes&amp;bg=666&amp;fg=444&amp;text=Segundo Slide" alt="Segundo Slide" />
+                    <div class=" content-banner carousel-caption d-none d-md-block">
+                        <img class="logo-slide"  src="assets/img/logo_ipes.png" alt="">
+                        <h1 class="title-slide">DOBRADA</h5>
+                        <p class="text-slide">AGORA O SONHO DA CASA <br> PRÓPRIA ESTÁ AO SEU ALCANCE</p>
+                        <button class="btn-slide">SAIBA MAIS</button>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+            </a>
+        </div>
+    </div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
