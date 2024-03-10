ea52daad07b736fcd4fa9583ab59f3752b416a02
Ulisses <ualbardeiro@gmail.com>
2024-03-09 21:45:54 -0300

    commit

--- index.php
+++ index.php
@@ -12,61 +12,73 @@
 <body>
 
 <?php include_once ("header.php") ?>    
+
+    <!-- carousel pc -->
         
-    <div class="bd-example">
-        <div id= "container  carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="banner carousel-item active">
-                    <img src="assets/img/banner1.png" class=" d-block w-100" data-src="holder.js/800x400?auto=yes&amp;bg=777&amp;fg=555&amp;text=Primeiro Slide" alt="Primeiro Slide" />
-                    <div class="
-                    content-banner carousel-caption d-none d-md-block">
-                        <img class="logo-slide" src="assets/img/logo_california.png" alt="">
-                        <h1 class="title-slide">santa HERNESTINA</h5>
-                        <p class="text-slide">AGORA O SONHO DA CASA <br> PRÓPRIA ESTÁ AO SEU ALCANCE</p>
-                        <button class="btn-slide">SAIBA MAIS</button>
-                    </div>
-                </div>
-                <div class="banner carousel-item">
-                    <img src="assets/img/banner2.png" class="d-block w-100" data-src="holder.js/800x400?auto=yes&amp;bg=666&amp;fg=444&amp;text=Segundo Slide" alt="Segundo Slide" />
-                    <div class=" content-banner carousel-caption d-none d-md-block">
-                        <img class="logo-slide"  src="assets/img/logo_ipes.png" alt="">
-                        <h1 class="title-slide">DOBRADA</h5>
-                        <p class="text-slide">AGORA O SONHO DA CASA <br> PRÓPRIA ESTÁ AO SEU ALCANCE</p>
-                        <button class="btn-slide">SAIBA MAIS</button>
-                    </div>
-                    
-                </div>
+    <div id="carouselExampleIndicators" class="carousel slide carousel-pc" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="assets/img/banner1.png" alt="Primeiro Slide">
             </div>
-            
-            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/banner2.png" alt="Segundo Slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Anterior</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Próximo</span>
-            </a>
-        </div>
+        </a>
     </div>
 
+    <!-- Carousel-Mobile -->
+
+    <div id="carouselExampleIndicators" class="carousel slide carousel-mobile" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="assets/img/banner1-mobile.png" alt="Primeiro Slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/banner2-mobile.png" alt="Segundo Slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+        </a>
+    </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+   
     <section>
         <div class="sobre">
             <h2>Quem somos</h2>
             <p>A Engeteg construtora é uma empresa que atua no ramo da construção civil, oferecendo soluções completas e integradas para os seus clientes.<br>A contrutora também se preocupa com o meio ambiente e a responsabilidade social, adotando práticas sistentáveis e apoiando projetos sociais nas comunidades onde atua. A Engeteg construtora é uma empresa que constrói mais do que obras, constrói sonhos</p>
-            <button class="btn-sobre">SAIBA MAIS</button>
+            <p>
+                <button class="btn-sobre">SAIBA MAIS</button>
+            </p>
         </div>
 
     </section>
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
