f4c4711c9c81ed94e5bb00979a258267a320c35b
Ulisses <ualbardeiro@gmail.com>
2024-03-11 15:12:24 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,16 +1,22 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 <link rel="stylesheet" href="assets/css/footer.css">
+
+
+      
+      
 
 <footer>
     <div class="footer">
-        <div>
+        <div class="contacts">
             <h3>Contatos</h3>
             <p>(19) 99936-4154</p>
             <p>E-mail: contato@engeteg.com.br</p>
         </div>
         <div>
-            risco
+            <div class="line"></div>
         </div>
         <div>
             <h3>Redes Sociais</h3>
@@ -18,8 +24,27 @@
             <i class="fa-brands fa-instagram"></i>
             <i class="fa-brands fa-whatsapp"></i>
         </div>
-        <div>
-            <img src="assets/img/logo-footer.png" alt="">
+        <div class="img">
+            <img src="assets/img/logo-footer.png" alt="logo">
         </div>
+    </div>
+    
+    <!-- FOOTER MOBILE -->
+
+    <div class="footer-mobile">
+        <div class="contacts">
+            <h3>Contatos</h3>
+            <p>(19) 99936-4154</p>
+            <p>E-mail: contato@engeteg.com.br</p>
+        </div>
+        <div class="social">
+            <h3>Redes Sociais</h3>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-whatsapp"></i>
+        </div>
+        <p class="img-mobile">
+            <img src="assets/img/logo-footer.png" alt="logo">
+        </p>
     </div>
 </footer>
