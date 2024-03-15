494b33e0bc54769333d4ddbdf6d17a6faad0e899
Ulisses <ualbardeiro@gmail.com>
2024-03-15 18:05:30 -0300

    commit

--- residencial-california.php
+++ residencial-california.php
@@ -4,13 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Residencial Califórnia</title>
+
+    <link rel="stylesheet" href="assets/css/residencial.css">
+
 </head>
 <body>
     <?php include_once("header.php");?>
 
-    <article class="text">
-        <div class="container"></div>
-    </article>
+    <div class="description">
+        <div class="block1">
+            <h2>ENGETEG EM SANTA ERNESTINA</h2>
+            <h1>Residencial Jardim Califórnia</h1>
+            <div class="line-horizontal"></div>
+            <p class="p">Você sabia que Santa Hernestina é uma cidade que tem tudo a ver com o café paulista, tem só 6.118 mil pessoas morando lá? Pois é, e agora voê pode ser uma delas com o Residencial Califórnia, o novo progeto da Engeteg.<br>
+        
+            Um bairro todo planejado e com tudo o que você precisa: água, esgotto, luz e asfalto. E as casas são ótimas: 42m² com dois quartos, sala, cozinha, banheiro e lavanderia coberta. E ainda tem espaço para você almentar a casa se quiser.<br>
+        
+            Isso é o que chamamos de qualidade de vida, conforto e independência. Fale com a gente! A Engeteg tem a melhor opção para você morar ou investir!</p>
+        </div>
+
+        <div class="block2">
+            <p class="p2">O sonho da casa própria nunca esteve tão perto!<br><br>
+        
+            Aproveite e veja as condições especiais que fizemos para você mudar de vida! Tem desconto, tem benefício do Minha Casa Minha Vida da Caiixa Econômica Federal.<br>
+            A Paz que você busca está aqui! Chega de pagar aluguel e invista no seu sonho. Comprar sua casa nova pode ser mais fácil do que você pensa!</p>
+        </div>
+        <div class="location">
+            <i class="fa-light fa-location-dot"></i>
+            <p>Santa Ernestina</p>
+        </div>
+    </div>
+
+    <div class="img-planta">
+        <div class="img-logo">
+             <img src="assets/img/logo-california.png" alt="">
+             <h4>Planta da Casa - Modelo 42m²</h4>
+             <p>Área de serviço <br>
+             2 Quartos <br>
+             Cozinha<br>
+             Banheiro<br>
+             Sala</p>
+        </div>
+        <div class="planta">
+            <img src="assets/img/planta.png" alt="">
+        </div>
+        
+    </div>
 
     <?php include_once("footer.php");?>
 </body>
--- residencial-ipes.php
+++ residencial-ipes.php
@@ -1,15 +1,98 @@
 <!DOCTYPE html>
-<html lang="pr-br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Residencial Califórnia</title>
+    <title>Residencial Jd dos Ipês II</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/residencial.css">
+
 </head>
 <body>
     <?php include_once("header.php");?>
 
-RESIDENCIAL ipes
+    <div class="description">
+        <div class="block1">
+            <h2>ENGETEG EM SANTA ERNESTINA</h2>
+            <h1>Residencial Jardim Califórnia</h1>
+            <div class="line-horizontal"></div>
+            <p class="p">Você sabia que Santa Hernestina é uma cidade que tem tudo a ver com o café paulista, tem só 6.118 mil pessoas morando lá? Pois é, e agora voê pode ser uma delas com o Residencial Califórnia, o novo progeto da Engeteg.<br>
+        
+            Um bairro todo planejado e com tudo o que você precisa: água, esgotto, luz e asfalto. E as casas são ótimas: 42m² com dois quartos, sala, cozinha, banheiro e lavanderia coberta. E ainda tem espaço para você almentar a casa se quiser.<br>
+        
+            Isso é o que chamamos de qualidade de vida, conforto e independência. Fale com a gente! A Engeteg tem a melhor opção para você morar ou investir!</p>
+        </div>
+
+        <div class="block2">
+            <p class="p2">O sonho da casa própria nunca esteve tão perto!<br><br>
+        
+            Aproveite e veja as condições especiais que fizemos para você mudar de vida! Tem desconto, tem benefício do Minha Casa Minha Vida da Caiixa Econômica Federal.<br>
+            A Paz que você busca está aqui! Chega de pagar aluguel e invista no seu sonho. Comprar sua casa nova pode ser mais fácil do que você pensa!</p>
+        </div>
+        <div class="location">
+            <i class="fa-light fa-location-dot"></i>
+            <p>Santa Ernestina</p>
+        </div>
+    </div>
+
+    <div class="img-planta">
+        <div class="img-logo">
+             <img src="assets/img/logo_ipes.png" alt="">
+             <h4>Planta da Casa - Modelo 42m²</h4>
+             <p>Área de serviço <br>
+             2 Quartos <br>
+             Cozinha<br>
+             Banheiro<br>
+             Sala</p>
+        </div>
+        <div class="planta">
+            <img src="assets/img/planta.png" alt="">
+        </div>
+        
+    </div>
+
+    
+    <div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active col-lg-4">
+        <img class="d-block w-100" src="assets/img/foto1.png" alt="Primeiro Slide">
+        </div>
+        <div class="carousel-item col-lg-4">
+        <img class="d-block w-100" src="assets/img/foto2.png" alt="Segundo Slide">
+        </div>
+        <div class="carousel-item col-lg-4">
+        <img class="d-block w-100" src="assets/img/foto3.png" alt="Terceiro Slide">
+        </div>
+        <div class="carousel-item col-lg-4">
+        <img class="d-block w-100" src="assets/img/foto4.png" alt="Segundo Slide">
+        </div>
+        <div class="carousel-item col-lg-4">
+        <img class="d-block w-100" src="assets/img/foto5.png" alt="Terceiro Slide">
+        </div>
+        <div class="carousel-item col-lg-4">
+        <img class="d-block w-100" src="assets/img/foto6.png" alt="Segundo Slide">
+        </div>
+        <div class="carousel-item col-lg-4">
+        <img class="d-block w-100" src="assets/img/foto7.png" alt="Terceiro Slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Próximo</span>
+    </a>
+</div>
 
     <?php include_once("footer.php");?>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
 </html>
